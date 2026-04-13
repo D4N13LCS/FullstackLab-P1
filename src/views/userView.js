473a0eadaa75a0abf51e85174890ddeea93a9708
D4N13LCS <danielmazzini6@gmail.com>
2026-04-13 01:25:35 -0300

    Feat: criação das views do user

--- src/views/userView.js
+++ src/views/userView.js
@@ -3,26 +3,14 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // route responsible for bringing all users registered
-router.get('/users', userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 
-router.get('/users/:id', userController.getUserById);
+router.get('/:id', userController.getUserById);
 
-router.post('/users/register', userController.registerUser);
+router.post('/register', userController.registerUser);
 
-router.put('/users/:id/edit', userController.updateUser);
+router.put('/:id/edit', userController.updateUser);
 
-router.delete('/users/:id/destroy', userController.deleteUserById);
-
-// route responsible for bringing an specific user registered
-// router.get('/:id');
-
-// route responsible for registering a new user
-// router.post('/');
-
-// route responsible for updating some information of an existent user
-// router.put('/');
-
-// route responsible for deleting an specific user
-// router.delete('/');
+router.delete('/:id/destroy', userController.deleteUserById);
 
 module.exports = router;
