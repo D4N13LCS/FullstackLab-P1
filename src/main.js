f1c7e819285742c6194e92200a66d7edbee62fe5
D4N13LCS <danielmazzini6@gmail.com>
2026-04-13 01:26:14 -0300

    Feat: adição das rotas dos posts

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const userView = require('./views/userView');
+const postView = require('./views/postView');
 
 app.use(express.json());
 
-app.use(userView)
+app.use("/users/", userView);
+
+app.use("/posts", postView);
 
 app.get('/', (req, res)=>{
     return res.json({'msg': 'Servidor está on'})
