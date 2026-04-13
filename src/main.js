ea4d7eb78dced36f8b389ad76455db2611d68b2c
D4N13LCS <danielmazzini6@gmail.com>
2026-04-13 00:54:41 -0300

    Feat: adição do roteamento para o endpoint /users

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const userView = require('./views/userView');
 
 app.use(express.json());
+
+app.use(userView)
 
 app.get('/', (req, res)=>{
     return res.json({'msg': 'Servidor está on'})
